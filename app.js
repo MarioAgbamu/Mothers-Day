f166c6298de081ed18c42bb9c7c41603f91e1faa
Agbamu - Kenoma Mario <106361322+MarioAgbamu@users.noreply.github.com>
2023-06-01 19:56:38 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+    // Set the end time to 5 seconds from now
+    const endTime = Date.now() + 7000;
+
+    // Check the time every 100 milliseconds
+    const intervalId = setInterval(() => {
+        // If the current time is past the end time, hide the loading animation and clear the interval
+        if (Date.now() > endTime) {
+            const loading = document.querySelector('.loading');
+            loading.style.display = 'none';
+            clearInterval(intervalId);
+        }
+    }, 100);
+    window.addEventListener("load", function () {
+  var loader = document.querySelector(".loading");
+});
+
 
